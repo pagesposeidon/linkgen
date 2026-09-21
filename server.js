cedc1d2454138bebe800a4e9e80baaf61b03e4a2
pagesposeidon <jennylovemorris@gmail.com>
2026-09-21 13:17:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
